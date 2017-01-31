39b2ce9f588bba341b1f2fcfd0bc5e1f35177ad6
suman dutta <sumandutta@sumans-mbp.torolab.ibm.com>
2017-01-31 15:19:04 -0500

    fix

--- test_full.c
+++ test_full.c
@@ -171,17 +171,6 @@ void test_syscall(int syscall) {
 	//clear_log();
 	do_intercept(syscall, 0);
 	do_start(syscall, 0, 0);
-	do_monitor(syscall);
-	do_stop(syscall, 0, 0);
-	do_start(syscall, 1, 0);
-	do_start(syscall, 2, 0);
-	do_monitor(syscall);
-	do_stop(syscall, 1, 0);
-	do_monitor(syscall);
-	do_stop(syscall, 2, 0);
-	do_monitor(syscall);
-	do_release(syscall, 0);
-
 }
 
 
