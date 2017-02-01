86ef57670254a65ed233c84090e64ef3e46af7ea
suman dutta <sumandutta@sumans-mbp.torolab.ibm.com>
2017-02-01 11:16:35 -0500

    fix

--- test_full.c
+++ test_full.c
@@ -169,9 +169,9 @@ int do_nonroot(int syscall) {
 void test_syscall(int syscall) {
 
 	//clear_log();
-	// do_intercept(syscall, 0);
+	 do_intercept(syscall, 0);
 	// do_intercept(syscall, -EBUSY);
-	// do_as_guest("./test_full nonroot %d", syscall, 0);
+	 do_as_guest("./test_full nonroot %d", syscall, 0);
 	// do_start(syscall, -2, -EINVAL);
 	// do_start(syscall, 0, 0);
 	// do_stop(syscall, 0, 0);
@@ -180,7 +180,7 @@ void test_syscall(int syscall) {
 	// do_stop(syscall, 1, 0);
 	// do_as_guest("./test_full start %d -1 %d", syscall, 0);
 	// do_stop(syscall, last_child, -EINVAL);
-	// do_release(syscall, 0);
+	do_release(syscall, 0);
 }
 
 
