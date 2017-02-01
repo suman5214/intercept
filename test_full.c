477687d4c65586e3d01ed37841841c5a9c694918
suman dutta <sumandutta@sumans-MacBook-Pro.local>
2017-01-31 23:43:52 -0500

    fix

--- test_full.c
+++ test_full.c
@@ -184,10 +184,6 @@ void test_syscall(int syscall) {
 		do_start(syscall, 1, 0);
 		do_start(syscall, 2, 0);
 		do_start(syscall, 3, 0);
-		do_stop(syscall,0,0);
-		do_start(syscall, 0, 0);
-		do_stop(syscall,getpid(),0);
-		do_monitor(syscall);
 		do_release(syscall,0);
 	}
 	
